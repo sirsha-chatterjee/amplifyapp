5c762b51452b841357577518a5b5058fa81450d3
schatterjee6 <Sirsha_Chatterjee@intuit.com>
2020-12-05 21:47:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sirsha Chatterjee hosted website
         </a>
       </header>
     </div>
